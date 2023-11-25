a609d2f1238777af65e6cd51c191e00d3fc2994c
Mariusz Trocewicz <throcu@gmail.com>
2023-11-25 01:24:20 +0100

    add: messages interface

--- src/app/room-list/room-chatbox/room-chatbox.component.ts
+++ src/app/room-list/room-chatbox/room-chatbox.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { Message } from '../../shared/interfaces/message.interface';
+// import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-room-chatbox',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './room-chatbox.component.html',
   styleUrl: './room-chatbox.component.css',
 })
-export class RoomChatboxComponent {
-  messages: object[] = [];
+export class RoomChatboxComponent implements OnInit {
+  messages: Message[] = [];
+  form!: FormGroup;
+  user!: any;
 
-  getMessages() {
+  constructor(
+    private loginService: LoginService // private apiService: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    // this.apiService.get().subscribe({
+    //   next: (messages) => {
+    //     console.log(messages);
+    //   },
+    // });
+    this.loginService.userName$.subscribe((name) => {
+      this.user = name;
+    });
+    this.form = new FormGroup({
+      message: new FormControl<string>(''),
+    });
+  }
+  onSubmit(form: FormGroup) {
+    this.messages.push({ ...this.user, ...form });
     console.log(this.messages);
+
+    // console.log({ ...this.user, ...form });
   }
 }
--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginService } from '../services/login.service';
 import { RouterLink } from '@angular/router';
+import { RoomChatboxComponent } from './room-chatbox/room-chatbox.component';
 
 @Component({
   selector: 'app-room-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RoomChatboxComponent],
   templateUrl: './room-list.component.html',
   styleUrl: './room-list.component.css',
 })
